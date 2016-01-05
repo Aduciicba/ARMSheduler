b53b229a764718d7ee24181bcbbd97cfb8e4d575
Aduciicba <sfera23@yandex.ru>
2016-01-06 00:39:54 +0300

    mail notifications

---
+++ ARMShedulerApp/ARMShedulerApp/DomainModels/Event.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ARMSchedulerApp
+{
+    public partial class Event 
+    {
+        List<EventEmail> _emailList = new List<EventEmail>();
+
+        public List<EventEmail> emailList
+        {
+            get
+            {
+                return _emailList;
+            }
+
+            set
+            {
+                _emailList = value;
+            }
+        }
+
+        
+    }
+}
